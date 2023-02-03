f5aae74fdb9a8bd19832aa3309f43c6824e44ac6
Mithun Tunga <mithunreddythunga@gmail.com>
2023-02-03 11:01:10 -0600

    adding script location and rest of the catalogue shell commands and adding catalogue service files

--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+scriptLocation=$(pwd)
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
 yum install nodejs -y
@@ -9,5 +10,20 @@ useradd roboshop
 mkdir /app
 
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-pwd
-exit
+
+cd /app
+
+npm install
+
+cp "${scriptLocation}"/files/catalogue.service /etc/systemd/system/catalogue.service
+
+systemctl deamon-reload
+
+systemctl enable catalogue
+systemctl start catalogue
+
+cp "${scriptLocation}"/files/mongo.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org-shell -y
+
+mongo --host 172.31.5.182 </app/schema/catalogue.js
