6ff4dc26fb9a9582b6df917d763e5a2a937b9ec3
Mithun Tunga <mithunreddythunga@gmail.com>
2023-02-06 16:38:59 -0600

    Adding shipping shell and service file

---
+++ shipping.sh
@@ -0,0 +1,30 @@
+scriptLocation=$(pwd)
+
+yum install maven -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+
+cd /app
+
+unzip /tmp/shipping.zip
+
+cd /app
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
+
+cp "${scriptLocation}"/files/shipping.service /etc/systemd/system/shipping.service
+
+systemctl daemon-reload
+
+systemctl enable shipping
+
+systemctl start shipping
+
+yum install mysql -y
+
+mysql -h 172.31.6.48 -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
