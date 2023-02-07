21e9edb08be30a6ff1022db99a038d2ee90cfeb9
Mithun Tunga <mithunreddythunga@gmail.com>
2023-02-06 22:49:04 -0600

    Adding -E to rabbitmq shell

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,8 +1,8 @@
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo -E bash
 
 yum install erlang -y
 
-curl -s https://packagecloud.io/install/repositories/raabitmq/rabbitmq-server/script.rpm.sh | sudo bash
+curl -s https://packagecloud.io/install/repositories/raabitmq/rabbitmq-server/script.rpm.sh | sudo -E bash
 
 yum install rabbitmq-server -y
 
